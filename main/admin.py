60c313b85085068c21665aec18014bef4fdf00c2
BekKing09 <satimovali91@gmail.com>
2026-03-02 10:37:02 +0500

    kurslar bilan ishlandi

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from main.models import Students
+from main.models import Students, Course
 
 
 
 admin.site.register(Students)
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "price", "discount", "discount_type", "final_price"]
+    list_display_links = ["id", "title"]
+    list_filter = ["price"]
+    search_fields = ["title"]
+
+admin.site.register(Course, CourseAdmin)
--- main/models.py
+++ main/models.py
@@ -7,14 +7,45 @@ class Students(models.Model):
     is_active = models.BooleanField(default=True)
     brith_data = models.DateField(null=True, blank=True)
 
+    Courses = models.ManyToManyField("main.Course", related_name="courses", blank=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)    
 
+    def courses_count(self):
+        return self.courses_count()
 
     def __str__(self):
-        return f"dtudent: {self.name} - #{self.id}"
+        return f"student: {self.name} - #{self.id}"
     
     class Meta:
         verbose_name = "O'quvchi"
         verbose_name_plural = "O'quvchilar"
+        ordering = ["id"]
+
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.PositiveIntegerField(default=0)
+    discount_type_choices = (
+        ("flex", "Aniq summa"),
+        ("percent", "Foizli")
+    )
+    discount_type = models.CharField(max_length=10, choices=discount_type_choices)
+    discount = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+    
+    def final_price(self):
+        if self.discount:
+            if self.discount_type == "percent":
+                return self.price - (self.price / 100 * self.discount)
+            return self.price - self.discount
+
+        return self.price
+    class Meta:
+        verbose_name = "Kurs"
+        verbose_name_plural = "Kurslar"
         ordering = ["id"]
